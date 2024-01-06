344240c52d3f69114c7bcbfef57144ec3926e258
Ilhambint <99519666+Ilhambint@users.noreply.github.com>
2024-01-07 02:19:59 +0700

    first

---
+++ src/Components/Burger/Bun.js
@@ -0,0 +1,38 @@
+import React from "react";
+import style from "./burger.module.css";
+
+const Bun = (props) => {
+  let bunClass;
+
+  switch (props.type) {
+    case "top":
+      bunClass = style.bun_top;
+      break;
+    case "insert":
+      bunClass = style.bun_insert;
+      break;
+    default:
+      bunClass = "";
+  }
+
+  return (
+    // a. Default styling (melalui CSS)
+    // <div className={`${style.bun} ${bunClass}`}></div>
+
+    // b. Inline class styling
+    // <div className={`${style.bun} ${bunClass}`} style={{ paddingLeft: '0px' }}></div>
+
+    // c. Styled components
+    // (Install dulu dengan npm install styled-components)
+
+    <div className={`${style.bun} ${bunClass}`}>
+      {/* <button> */}
+      {/* <StyledButton>
+        Button
+      </StyledButton> */}
+      {/* </button> */}
+    </div>
+  );
+};
+
+export default Bun;
